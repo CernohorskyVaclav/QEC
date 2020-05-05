71a5d9caa4840870c80ef9002ed12dab1bdb601b
CernohorskyVaclav <vaclav.cernohorsky@gmail.com>
2020-05-05 15:03:25 +0200

    Initial Commit

---
+++ QEC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QEC
+//
+//  Created by Václav Černohorský on 05/05/2020.
+//  Copyright © 2020 Václav Černohorský. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
